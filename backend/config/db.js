3aadce7f45fbf44332b54180b0c9cb77e79c9083
Samarth Joshi <127830200+samarthjoshi2512@users.noreply.github.com>
2024-11-12 23:33:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://samarthjoshi81:samsjj2512@cluster0.u3sca.mongodb.net/Projects').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://samarthjoshi81:samsjj2512@cluster0.u3sca.mongodb.net/Projects?retryWrites=true&w=majority&ssl=true').then(()=>console.log("DB Connected"));
 }
 
 
